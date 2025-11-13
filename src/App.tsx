fb39ded8166d23118807be5487b59f0ebde63074
krearaujo <karenohanaaraujo@gmail.com>
2025-11-13 20:07:08 -0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Header from "./Components/Header";
+import Sidenav from "./Components/Sidenav";
+import { DataContextProvider } from "./Context/DataContext";
+import Resume from "./Pages/Resume";
+import "./Style.css";
+
+function App() {
+  return (
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resume />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
+}
+
+export default App;
---
+++ src/Hooks/useFetch.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+function useFetch<T>(
+  url: RequestInfo | URL,
+  options?: RequestInit
+): FetchState<T> {
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState<T | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+
+  const optionsRef = React.useRef(options);
+  optionsRef.current = options;
+
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await fetch(url, {
+          ...optionsRef.current,
+          signal: controller.signal,
+        });
+        if (!response.ok) {
+          throw new Error("Erro ao buscar dados");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (erro) {
+        setError((erro as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
