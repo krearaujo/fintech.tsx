73da6a4c300c5d853c2e8aede2eb4055b830988d
krearaujo <karenohanaaraujo@gmail.com>
2025-12-04 11:59:08 -0300

    add side nav and styles

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,15 @@
 import DateRange from "./DateRange";
 import Months from "./Months";
+import React from "react";
 
 const Header = () => {
+  const [title, setTitle] = React.useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="dateRange mb">
         <DateRange />
+        <h1 className="box">{title}</h1>
       </div>
       <Months />
     </header>
--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import fintech from "../assets/fintech.svg";
+import resume from "../assets/icons/resumo.svg";
+import sales from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import config from "../assets/icons/configuracoes.svg";
+import contact from "../assets/icons/contato.svg";
+import logout from "../assets/icons/sair.svg";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <img src={fintech} alt="Fintech logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resume} />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={sales} />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} />
+          </span>
+          <a href="">Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={config} />
+          </span>
+          <a href="">Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contact} />
+          </span>
+          <a href="">Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={logout} />
+          </span>
+          <a href="">Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
